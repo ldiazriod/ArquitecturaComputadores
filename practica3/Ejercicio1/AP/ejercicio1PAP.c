ca072ef27bb1073e5ba2411d0c1f438c8ad30528
ldiazriod <ldiazriod@alumnos.nebrija.es>
2021-12-01 10:11:37 +0100

    MPI compiled files deleted

--- practica3/Ejercicio1/AP/ejercicio1PAP.c
+++ practica3/Ejercicio1/AP/ejercicio1PAP.c
@@ -1,35 +1,6 @@
 #include "mpi.h"
 #include <stdio.h>
 
-/*
- __________________________________________
-|                                          |
-| Ejercicio 1: Comunicación Punto a punto  |
-|__________________________________________|
-
-*/
-
-/*
-    Planteamiento: 
-
-    Para este ejercicio la cominicación punto a punto es un poco distinta. No vamos mandando la información de un proceso al otro, si no que la información
-    se manda desde un nodo inicial o padre a todos los procesos. 
-
-    Para resolver esto, podemos basarnos parcialmente en el planteamiento del ejercicio 3 de la práctica 2. La principal diferencia es que el proceso 0 es
-    el único que manda el mensaje, y el resto de procesos únicamente reciben la información y no la mandan a ninguna parte.
-
-    Esto supone el problema inicial en el que debemos mandar la información a todos los procesos desde el proceso 0. Para ello la mejor solución que se me 
-    ha ocurrido es utilizar un bucle "for" con el que mandar a cada proceso correspondiente un mensaje. 
-
-    La función MPI_Send recibiría como parametros el valor inicial modificado, el tamaño, el tipo de dato, el número de iteración (i) y el comm. Para esto,
-    el bucle "for" empezaría en i=1 (Para empezara a mandar el mensaje al proceso 1) e iría aumentado hasta llegar al número de procesos-1 (El último proceso).
-
-    El resto de procesos reciben la información y la imprimen.
-
-    Este ejercicio es sencillo
-
-*/
-
 int main(int argc, char** argv){
     int rank;
     int size;
@@ -63,3 +34,4 @@ int main(int argc, char** argv){
     MPI_Finalize();
     return 0;
 }
+
--- practica3/Ejercicio1/BC/ejercicio1Col.c
+++ practica3/Ejercicio1/BC/ejercicio1Col.c
@@ -1,29 +1,6 @@
 #include "mpi.h"
 #include <stdio.h>
 
-
-/*
- ______________________________________
-|                                      |
-| Ejercicio 1: Comunicación Colectiva  |
-|______________________________________|
-
-*/
-
-/*
-    Planteamiento: 
-
-    Este ejercicio es incluso más fácil que el anterior. Vamos a utilizar MPI_Bcast, que manda el mensaje desde el proceso padre (proceso 0 en este caso)
-    al resto de procesos.
-
-    La parte más compleja de este ejercicio es entender que cuando utilizamos MPI_Bcast, el mensaje que reciben los procesos se reciben mediante la propia
-    función de MPI_Bcast, por lo que todos los procesos deben ejecutar esta función. De esta manera nos quitamos el if-else y nos quedamos, en el caso de
-    que queramos que el mensaje lo añada el usuario por pantalla o queramos modificar el mensaje en algún proceso en especifico, únicamente con el if.
-
-*/
-
-
-
 int main(int argc, char** argv){
     int rank;
     int msg;
@@ -45,3 +22,4 @@ int main(int argc, char** argv){
     MPI_Finalize();
     return 0;
 }
+
